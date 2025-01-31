afb38ebb610ad1f255c0fac439ede81e30fbddc1
Adam Arutyunov <adam.arut@gmail.com>
2025-01-31 09:08:52 +0000

    NestedTable columns count injection

--- client/src/components/ui/NestedTable/NestedTable.tsx
+++ client/src/components/ui/NestedTable/NestedTable.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { CSSProperties, ReactNode, useMemo } from 'react'
 
 import './NestedTable.scss'
 
@@ -8,8 +8,18 @@ interface NestedTableProps {
 }
 
 export default function NestedTable({ columnsLabels, children }: NestedTableProps) {
+    const columnsCount: number = useMemo(() => columnsLabels.length, [columnsLabels])
+
+    // Injecting columns count in order to be used
+    // by all NestedTableRows
+    const tableStyle = useMemo(() => {
+        return {
+            '--table-columns-count': columnsCount,
+        } as CSSProperties
+    }, [columnsCount])
+
     return (
-        <div className="NestedTable">
+        <div className="NestedTable" style={tableStyle}>
             <div className="NestedTableRow">
                 <div className="NestedTableRow_content NestedTableRow_labels">
                     {/* Empty div to fill first grid cell */}
